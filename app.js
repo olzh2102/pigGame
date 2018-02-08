f5884e6d893e60722e87a148e5a9e92a85a989b7
olzhas2102 <olzhas2102@yahoo.com>
2018-02-08 12:43:37 +0600

    Rolling dice and holding current score

--- app.js
+++ app.js
@@ -9,14 +9,45 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1;
-
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+var buttonRoll = document.querySelector('.btn-roll');
+buttonRoll.addEventListener('click', function() {
+	// 1. Random number
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	//2. Display the result
+	var diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+
+	//3. Update the score IF the rolled number is NOT 1
+	if (dice !== 1) {
+		// Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		// Next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+
+		diceDOM.style.display = 'none';
+	}
+})
